fe8c1be9928b7621cb2f948f78674edbf9836fc2
10000sun <86609512+10000sun@users.noreply.github.com>
2023-10-08 15:45:50 +0900

    bikeaccident

---
+++ js/js.js
@@ -0,0 +1,5 @@
+const API_KEY='vDX%2FUI2gzJeT6CJM1otu39l%2Byx0prw9aeTusQVSZCK9QkcGOX9oViiRL8uZLh7ftTKTxfEjlAKtdRpbG%2BSk3WA%3D%3D';
+
+function getData(){
+    const url= 'https://api.odcloud.kr/api//15094182/v1/uddi:d1d724be-2133-47f8-a445-9f3ef7f9c5a6'
+}
